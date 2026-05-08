e829b82a9ddf2cba38b420b4c277c71df8b7a2b1
Eternal161 <daulam1601@gmail.com>
2026-05-09 00:36:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ GITHUB_TOKEN = os.getenv("GH_TOKEN")
 REPO_NAME = os.getenv("GH_REPO", "Eternal161/dausoco")
 FILE_PATH = "bongda.m3u"
 WAITING_VIDEO_URL = "https://example.com/video-cho.mp4"
-LIMIT_MATCHES = 30
+LIMIT_MATCHES = 3
 
 # Cache logo để tránh gọi API nhiều lần
 LOGO_CACHE = {}
